f1bee6959d53554097e0d34d37edd9ed8d06a188
Breno Menezes <brenoma@live.com>
2022-04-08 17:00:53 -0300

    Project Initialization

---
+++ librabbitmq/src/main/java/constants/RabbitMQConstants.java
@@ -0,0 +1,5 @@
+package constants;
+
+public class RabbitMQConstants {
+    public static final String QUEUE_SUBSCRIPTION = "SUBSCRIPTION";
+}
---
+++ subsapi/src/main/java/br/com/atlantico/brenoararipe/subsapi/connections/RabbitMQConnection.java
@@ -0,0 +1,49 @@
+package br.com.atlantico.brenoararipe.subsapi.connections;
+
+import constants.RabbitMQConstants;
+import org.springframework.amqp.core.AmqpAdmin;
+import org.springframework.amqp.core.Binding;
+import org.springframework.amqp.core.DirectExchange;
+import org.springframework.amqp.core.Queue;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+import javax.annotation.PostConstruct;
+
+@Component
+public class RabbitMQConnection {
+    private static final String EXCHANGE_NAME = "amq.direct";
+
+    @Autowired
+    private AmqpAdmin amqpAdmin;
+
+    private Queue queue(String queueName) {
+        return new Queue(queueName, true, false, false);
+    }
+
+    private DirectExchange directExchange() {
+        return new DirectExchange(EXCHANGE_NAME);
+    }
+
+    private Binding binding(Queue queue, DirectExchange exchange) {
+        return new Binding(queue.getName(), Binding.DestinationType.QUEUE, exchange.getName(), queue.getName(), null);
+    }
+
+    @PostConstruct
+    private void add() {
+        Queue queueSubscription = this.queue(RabbitMQConstants.QUEUE_SUBSCRIPTION);
+
+        DirectExchange exchange = this.directExchange();
+
+        Binding bindPurchased = this.binding(queueSubscription, exchange);
+
+        // Creating the queues on RabbitMQ
+        this.amqpAdmin.declareQueue(queueSubscription);
+
+        // Creating the exchange on RabbitMQ
+        this.amqpAdmin.declareExchange(exchange);
+
+        // Creating bindings between queue and exchange
+        this.amqpAdmin.declareBinding(bindPurchased);
+    }
+}
