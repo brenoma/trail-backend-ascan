dac3e6616eeeaeaf5f5472b7ee8a4fd372c7be25
Breno Menezes <brenoma@live.com>
2022-05-10 00:20:21 -0300

    OAuth2 security added

--- subs-api/src/main/java/br/com/atlantico/brenoararipe/subsapi/config/SecurityConfig.java
+++ subs-api/src/main/java/br/com/atlantico/brenoararipe/subsapi/config/SecurityConfig.java
@@ -1,42 +1,85 @@
 package br.com.atlantico.brenoararipe.subsapi.config;
 
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-import org.springframework.security.crypto.factory.PasswordEncoderFactories;
-import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.oauth2.server.resource.authentication.JwtAuthenticationConverter;
+import org.springframework.security.oauth2.server.resource.authentication.JwtGrantedAuthoritiesConverter;
 
 @Configuration
 @EnableWebSecurity
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
-    @Override
-    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
-        PasswordEncoder encoder =
-                PasswordEncoderFactories.createDelegatingPasswordEncoder();
-        auth
-                .inMemoryAuthentication()
-                .withUser("user")
-                .password(encoder.encode("user"))
-                .roles("USER")
-                .and()
-                .withUser("admin")
-                .password(encoder.encode("admin"))
-                .roles("USER", "ADMIN");
-    }
+//    @Override
+//    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+//        PasswordEncoder encoder =
+//                PasswordEncoderFactories.createDelegatingPasswordEncoder();
+//        auth
+//                .inMemoryAuthentication()
+//                .withUser("user")
+//                .password(encoder.encode("user"))
+//                .roles("USER")
+//                .and()
+//                .withUser("admin")
+//                .password(encoder.encode("admin"))
+//                .roles("USER", "ADMIN");
+//    }
+//
+//    @Override
+//    protected void configure(HttpSecurity http) throws Exception {
+//        http
+//                .authorizeRequests()
+//                .anyRequest()
+//                .authenticated()
+//                .and()
+//                .httpBasic()
+//                .and()
+//                .csrf()
+//                .disable();
+//    }
+
+//    @Autowired
+//    private AuthenticationManager authenticationManager;
+//
+//    @Autowired
+//    private UserDetailsService customUserDetailsService;
+//
+//    @Override
+//    @Bean
+//    public AuthenticationManager authenticationManagerBean() throws Exception {
+//        return super.authenticationManagerBean();
+//    }
+//
+//    @Override
+//    protected void configure(AuthenticationManagerBuilder auth) throws Exception
+//    {
+//        auth.parentAuthenticationManager(authenticationManagerBean())
+//                .userDetailsService(customUserDetailsService);
+//    }
 
     @Override
-    protected void configure(HttpSecurity http) throws Exception {
-        http
-                .authorizeRequests()
-                .anyRequest()
-                .authenticated()
+    public void configure(HttpSecurity http) throws Exception {
+        http.cors()
                 .and()
-                .httpBasic()
+                    .authorizeRequests()
+                    .antMatchers(HttpMethod.PUT, "/api/v1/subscription")
+                    .hasAuthority("USER")
+                    .anyRequest()
+                    .authenticated()
                 .and()
-                .csrf()
-                .disable();
+                    .oauth2ResourceServer()
+                    .jwt()
+                    .jwtAuthenticationConverter(getJwtAuthenticationConverter());
+    }
+
+    JwtAuthenticationConverter getJwtAuthenticationConverter() {
+        JwtGrantedAuthoritiesConverter converter = new JwtGrantedAuthoritiesConverter();
+        converter.setAuthoritiesClaimName("authorities");
+        converter.setAuthorityPrefix("");
+        JwtAuthenticationConverter authenticationConverter = new JwtAuthenticationConverter();
+        authenticationConverter.setJwtGrantedAuthoritiesConverter(converter);
+        return authenticationConverter;
     }
 }
